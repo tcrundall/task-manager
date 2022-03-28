4294aa16d94653bc9907a37eaebeb43cf520a00a
Timothy Crundall <u5018130@anu.edu.au>
2022-03-28 23:36:24 +1100

    Add comments to (Draft)Todo

--- src/main/java/com/company/taskmanager/api/Todo.java
+++ src/main/java/com/company/taskmanager/api/Todo.java
@@ -5,6 +5,13 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.ArrayList;
 import java.util.Optional;
 
+/**
+ * Represents a Todo item.
+ * Todos are created solely by the server. This is done
+ * to ensure correct assignment of unique IDs.
+ * Todos can be created either from a draftTodo object
+ * or from just a name.
+ */
 public class Todo {
     private long id;
     private String name;
@@ -15,6 +22,14 @@ public class Todo {
         // Jackson deserialization
     }
 
+    /**
+     * Creates a Todo object from a draftTodo object
+     * resulting in a Todo object with an id, a name
+     * an optional description, and a arraylist of tasks.
+     * @param id        The id number of the Todo object
+     *                  This is assumed to be unique.
+     * @param draftTodo The draftTodo which will be cloned.
+     */
     public Todo(long id, DraftTodo draftTodo) {
         this.id = id;
         this.name = draftTodo.getName();
@@ -22,9 +37,17 @@ public class Todo {
         this.tasks = draftTodo.getTasks();
     }
 
-    public Todo(long id, String name) {
+    /**
+     * Creates a todo with given name and an optional description.
+     * For testing purposes we auto-generate a list of dummy tasks
+     * @param name          The name of the draftTodo.
+     *                      Ideally short and simple. Spaces are permitted
+     * @param description   The description of the draftTodo. As long as necessary.
+     */
+    public Todo(long id, String name, Optional<String> description) {
         this.id = id;
         this.name = name;
+        this.description = description;
         tasks = new ArrayList<>();
         for (long i=0;i<3;i++) {
             tasks.add(new Task(i, "dummy task "+i, Optional.of("dummy description")));
